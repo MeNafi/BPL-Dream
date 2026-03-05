85d805199a9e81c3c52bacf2c780fa0a3e25de9d
MeNafi <nayeefsarkernafi@gmail.com>
2026-03-06 02:21:09 +0600

    button color shifted when toggle

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,10 @@ function App() {
         <h1 className="font-bold text-2xl">Available players</h1>
 
         <div className="font-bold">
-          <button onClick={()=>setToggle(true)} className="py-3 px-4 border-1 border-gray-400 rounded-l-2xl border-r-0 bg-[#E7FE29]">
+          <button onClick={()=>setToggle(true)} className={`py-3 px-4 border-1 border-gray-400 rounded-l-2xl border-r-0 ${toggle === true? "bg-[#E7FE29]":""}`}>
             Available
           </button>
-          <button onClick={()=>setToggle(false)} className="py-3 px-4 border-1 border-gray-400 rounded-r-2xl border-l-0">
+          <button onClick={()=>setToggle(false)} className={`py-3 px-4 border-1 border-gray-400 rounded-r-2xl border-l-0 ${toggle === false? "bg-[#E7FE29]":""}`}>
             selected <span>{0}</span>
           </button>
         </div>
