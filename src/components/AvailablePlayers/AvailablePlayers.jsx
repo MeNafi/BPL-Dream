be4a55dc3fc32d515719f39dff80eaef3c1081a9
MeNafi <nayeefsarkernafi@gmail.com>
2026-03-05 21:11:47 +0600

    first player card all things done

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -1,29 +1,51 @@
-import React, { use } from 'react';
+import React, { use } from "react";
+import userImg from "../../assets/user-1.png";
+import flagImg from "../../assets/report-1.png";
 
-const AvailablePlayers = ({playersPromise}) => {
-    
-    const playerData = use(playersPromise)
-    console.log(playerData)
-    return (
-        <div className="max-w-[1000px] mx-auto">
-          <div className="card bg-base-100 w-96 shadow-sm p-4">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
- 
-</div>
-         </div> 
-        
+const AvailablePlayers = ({ playersPromise }) => {
+  const playerData = use(playersPromise);
+  console.log(playerData);
+  return (
+    <div className="max-w-[1000px] mx-auto">
+      <div className="card bg-base-100 w-96 shadow-sm p-4">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="mt-4">
+          <div className="flex">
+            <img src={userImg} alt="" />
+            <h2 className="card-title ml-2">Virat Kohli</h2>
+          </div>
+
+          <div className="flex justify-between mt-4 border-b-2 border-gray-200 pb-1">
+            <div className="flex item-center ">
+              <img className="w-[20px] h-[20px]" src={flagImg} alt="" />
+              <span className="ml-2">India</span>
+            </div>
+            <button className="btn">All rounder</button>
+          </div>
+
+          <div className="flex justify-between font-bold">
+            <span>Rating</span>
+            <span>5</span>
+          </div>
+
+          <div className="flex justify-between mt-4">
+            <span className="font-bold">Left hand bat</span>
+            <span>Right hand ball</span>
+          </div>
+         
+          <div className="card-actions mt-4 flex justify-between-items-center">
+            <p className="font-bold">Price: $1500000</p>
+            <button className="btn btn-primary">Choose player</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AvailablePlayers;
