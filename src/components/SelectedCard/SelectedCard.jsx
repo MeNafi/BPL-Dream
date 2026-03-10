d1fa3f75042e5c87a7ca15631ea0e7b433ae4b1c
MeNafi <nayeefsarkernafi@gmail.com>
2026-03-10 23:09:56 +0600

    fixed bugs in the SelectedCard.jsx section

--- src/components/SelectedCard/SelectedCard.jsx
+++ src/components/SelectedCard/SelectedCard.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 const SelectedCard = ({player}) => {
     console.log(player)
     return (
-         <div className="border-2 border-gray-100 p-3 flex justify-between items-center rounded-xl">
+         <div className="border-2 mt-5 border-gray-100 p-3 flex justify-between items-center rounded-xl">
      
      <div className="flex items-center">
-        <img src= {} className="h-[50px] w-[50px] rounded-xl" alt="" />
+        <img src= {player["player-image"]} className="h-[50px] w-[50px] rounded-lg" alt="" />
     
      <div className="ml-2">
-        <h1>{}</h1>
-        <p className="text-xs">Left-hand bat</p>
+        <h1>{player["player-name"]}</h1>
+        <p className="text-xs">{player["player-role"]}</p>
      </div>
       </div>
       <div>
-      <img src="https://i.ibb.co.com/5xMKcMxD/delete.png" alt="delete vector icon" />
+      <img className="w-5 h-5" src="https://i.ibb.co.com/5xMKcMxD/delete.png" alt="delete vector icon" />
       </div>
       </div>
     );
